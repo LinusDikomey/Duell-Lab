911ee56caf2074c9f186a75f07cb5eefa4bdf222
Linus <a@b>
2018-06-26 13:34:09 +0200

    Update: Collision methods

--- Spiel/src/entities/Entity.java
+++ Spiel/src/entities/Entity.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -7,17 +8,28 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import renderer.Loader;
+import world.Tickable;
 
-public abstract class Entity {
+public abstract class Entity implements Tickable {
 
 	public int x, y;
+	public int lastX, lastY;
 	private BufferedImage texture;
 	protected Loader loader;
+	public Rectangle collisionBox = new Rectangle(0, 0, 100, 100);
+	boolean collidable = true;
 	
 	public Entity(int x, int y, Loader loader) {
 		this.x = x;
 		this.y = y;
 		this.loader = loader;
+	}
+	
+	public void checkCollision(Rectangle r) {
+		if(collidable) {
+			Rectangle collisionRect = new Rectangle(x + collisionBox.x, y + collisionBox.y, collisionBox.width, collisionBox.height);
+			if(collisionRect.intersects(r));
+		}
 	}
 	
 	protected void loadTexture(String name) {
@@ -26,6 +38,19 @@ public abstract class Entity {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	public void tick() {
+		saveCoords();
+	}
+	
+	/**
+	 * Sollte am Ende jedes Ticks aufgerufen werden
+	 */
+	private void saveCoords() {
+		lastX = x;
+		lastY = y;
 	}
 	
 	public BufferedImage getTexture() {
